a7d27e13882027074fde8623141def1dc57f7c44
s4pphireek <74686481+s4pphireek@users.noreply.github.com>
2023-01-11 17:33:46 +0100

    Update cisco.sh

--- cisco.sh
+++ cisco.sh
@@ -5,7 +5,7 @@ echo "Changing wifi password in Cisco AP~~ by sapphire"
 
 haslo=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w 12 | head -n 1) 
 echo $haslo > file.txt
-echo "Wygnerowano haslo: " $haslo
+echo "Password generated: " $haslo
 
 ./wlc.exp <device_IP> <user> <password>
 
